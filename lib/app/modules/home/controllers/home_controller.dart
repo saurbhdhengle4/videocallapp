efd74fef9d59fce24f01e536f1c5db8365ab7321
Saurabh-dev199 <saurabh.d@olatechs.com>
2025-09-25 11:09:01 +0530

    splash added

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,11 +1,10 @@
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
-  //TODO: Implement HomeController
-
-  final count = 0.obs;
   @override
   void onInit() {
+    initialization();
     super.onInit();
   }
 
@@ -19,5 +18,10 @@ class HomeController extends GetxController {
     super.onClose();
   }
 
-  void increment() => count.value++;
+  void initialization() async {
+    await Future.delayed(const Duration(seconds: 1));
+    await Future.delayed(const Duration(seconds: 1));
+    await Future.delayed(const Duration(seconds: 1));
+    FlutterNativeSplash.remove();
+  }
 }
