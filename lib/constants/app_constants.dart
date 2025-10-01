75676d61ca6a2e8f908b20eceb4ecdee7aeded81
Saurbh Govind Dhengle <75467003+saurbhdhengle4@users.noreply.github.com>
2025-10-01 12:46:48 +0530

    Update app_constants.dart

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -1,11 +1,7 @@
 class AppConstants {
-  // static String get appId => dotenv.env['APP_ID'] ?? '';
-  // static String get channelName => dotenv.env['CHANNEL_NAME'] ?? '';
-  // static String get tempToken => dotenv.env['TEMP_TOKEN'] ?? '';
-  static String get appId => '659498f07ad04708bc1b9ff18ae44209';
-  static String get channelName => 'testapp123';
-  static String get tempToken =>
-      "007eJxTYDi9LrFpQoLz4tUnNxk4v+DrfXXl24TZ86cynCkQDzbSv8CowGBmamliaZFmYJ6YYmBibmCRlGyYZJmWZmiRmGpiYmRgmbz4ekZDICPDQ4kdrIwMEAjiczGUpBaXJBYUGBoZMzAAAF5ZIis=";
+   static String get appId => dotenv.env['APP_ID'] ?? '';
+   static String get channelName => dotenv.env['CHANNEL_NAME'] ?? '';
+   static String get tempToken => dotenv.env['TEMP_TOKEN'] ?? '' 
 }
 /// AppConstants loads keys from the .env file.
 ///  Create a .env file in your project root 
